1ecda9865d2648567911971500a722d970eab468
Aitor <aitordegomar91@hotmail.com>
2020-11-09 18:51:08 +0100

    Router and Link in NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import NavBar from './componets/NavBar';
+import About from './componets/About';
+import Home from './componets/Home';
+
 
 function App() {
   return (
     <>
-<p>lorem</p>
+    <Router>
+    <NavBar/>
+    <Switch>
+      <Route path='/' exact><Home /></Route>
+      <Route path='/about'><About /></Route>
+      </Switch>
+</Router>
     </>
   );
 }
