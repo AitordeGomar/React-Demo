ae5e3705fc9fe33393341a5b9852f1bab5ac9050
Aitor <aitordegomar91@hotmail.com>
2020-11-10 19:57:57 +0100

    randomize

--- src/componets/Tournament.js
+++ src/componets/Tournament.js
@@ -2,14 +2,42 @@ import React,  {useState} from 'react';
 import Data from './Data';
 
 const Tournament = () => {
-
-    const [valueA, setValueA] = useState(Data[0]);
-    const [valueB, setValueB] = useState(Data[1]);
-    const [valueC, setValueC] = useState(Data[2]);
-    const [valueD, setValueD] = useState(Data[3]);
+    const [valueA, setValueA] = useState({});
+    const [valueB, setValueB] = useState({});
+    const [valueC, setValueC] = useState({});
+    const [valueD, setValueD] = useState({});
     const [valueE, setValueE] = useState({});
     const [valueF, setValueF] = useState({});
     const [valueG, setValueG] = useState({});
+    const random =()=>{
+        setValueE({name:'', power:''});
+        setValueF({name:'', power:''});
+        setValueG({name:'', power:''});
+if(Math.random()<0.25){
+    setValueA(Data[0]);
+    setValueB(Data[1]);
+    setValueC(Data[2]);
+    setValueD(Data[3]);
+}else if(Math.random()>0.25 && Math.random()<0.5){
+    setValueA(Data[1]);
+    setValueB(Data[2]);
+    setValueC(Data[3]);
+    setValueD(Data[0]);
+}else if(Math.random()>0.5 && Math.random()<0.75){
+    setValueA(Data[2]);
+    setValueB(Data[3]);
+    setValueC(Data[0]);
+    setValueD(Data[1]);
+}else if(Math.random()>0.75){
+    setValueA(Data[3]);
+    setValueB(Data[0]);
+    setValueC(Data[1]);
+    setValueD(Data[2]);
+}
+    }
+
+
+
     const reset =(e)=>{
         e.preventDefault();
         setValueE({name:'', power:''});
@@ -17,20 +45,15 @@ const Tournament = () => {
         setValueG({name:'', power:''});
     }
 
-    const test =()=>{
-
-        console.log(Math.random()*2);
-        console.log(Math.abs(valueA.power-Math.random()*2));
-        console.log(Math.abs(valueB.power-Math.random()*2));
-    }
+    
 
     return (
         <>
-        <button onClick={test}>test</button>
+        <button onClick={random}>Randomize</button>
         <h3>Semifinal</h3>
         <div>
-        <input value={valueA.name} readOnly/>
-        <input value={valueB.name} readOnly/>
+        <input value={valueA.name || ''} readOnly/>
+        <input value={valueB.name || ''} readOnly/>
         <button onClick={(e)=>{
         e.preventDefault();
         if(Math.abs(valueA.power-Math.random()*2) < Math.abs(valueB.power-Math.random()*2)){
