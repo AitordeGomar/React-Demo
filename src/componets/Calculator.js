7291fe211f8a2f1828b20d0cb62e94d362bfe8f7
Aitor <aitordegomar91@hotmail.com>
2020-11-23 15:43:34 +0100

    buttons in same file and css imported

--- src/componets/Calculator.js
+++ src/componets/Calculator.js
@@ -1,14 +1,41 @@
 import React, {useState} from 'react';
-import CalcButton from './CalcButton';
+import './css/Calculator.css';
 
 const Calculator = () => {
 
     const [value, setValue] = useState('');
+
     return (
         <div>
             <input type='text' value={value}/>
-            <CalcButton />
+            <CalcButton symbol='C' width='cols-3'/>
+            <CalcButton symbol='/'/>
+            <CalcButton symbol='9'/>            
+            <CalcButton symbol='*'/>            
+            <CalcButton symbol='8'/>
+            <CalcButton symbol='7'/>
+            <CalcButton symbol='6'/>            
+            <CalcButton symbol='-'/>            
+            <CalcButton symbol='5'/>
+            <CalcButton symbol='4'/>
+            <CalcButton symbol='3'/>
+            <CalcButton symbol='+'/>
+            <CalcButton symbol='2'/>
+            <CalcButton symbol='1'/>
+            <CalcButton symbol='0'/>
+            <CalcButton symbol='.'/>            
+            <CalcButton symbol='='/>            
+
         </div>
+    )
+
+    
+}
+const CalcButton = (props)=>{
+    return(
+        <>
+        <button className={props.width}>{props.symbol}</button>
+        </>
     )
 }
 
