beb2d4ad759cee80c99f078df4ae76afb4504703
Aitor <aitordegomar91@hotmail.com>
2020-11-23 17:36:53 +0100

    calculate added and alerting whether no second value

--- src/componets/Calculator.js
+++ src/componets/Calculator.js
@@ -2,15 +2,38 @@ import React, {useState} from 'react';
 import './css/Calculator.css';
 
 const Calculator = () => {
-    const [defaultValue] = useState('0');
+    const [defaultValue] = useState('');
     const [value, setValue] = useState(defaultValue);
+    const [savedValue, setSavedValue] = useState('');
+    const [savedOperation, setSavedOperation] = useState('');
     const reset = ()=>{
-        return setValue('0')
+        setValue('');
+        setSavedValue('');
     
     }
-
     const addValue= (e)=>{
+         if(e.target.innerText === 'C' || e.target.innerText === '/' || e.target.innerText === 'X' || e.target.innerText === '-' || e.target.innerText === '+'){
+             setSavedValue(value);
+             setSavedOperation(e.target.innerText);
+            setValue('');
+        }else{
         setValue(value + e.target.innerText);
+        }
+    } 
+    const calculate = ()=>{
+        if(value === ''){alert('You need the second value');}else{
+        switch(savedOperation){
+            case '/': setValue(parseFloat(savedValue) / parseFloat(value));
+            break;
+            case 'X': setValue(parseFloat(savedValue) * parseFloat(value));
+            break;
+            case '+': setValue(parseFloat(savedValue) + parseFloat(value));
+            break;
+            case '-': setValue(parseFloat(savedValue) - parseFloat(value));
+            break;
+            default: setValue('0');
+        }
+    }
     }
 
     const buttons = [
@@ -30,11 +53,12 @@ const Calculator = () => {
         {symbol:'+', width:'cols-1', action:addValue},
         {symbol:'0', width:'cols-2', action:addValue},
         {symbol:'.', width:'cols-1', action:addValue},
-        {symbol:'=', width:'cols-1', action:addValue}
+        {symbol:'=', width:'cols-1', action:calculate}
     ]
 
     return (
         <div>
+            <input type='text' value={savedValue || ''} readOnly/>
             <input type='text' value={defaultValue || value} readOnly/>
             {buttons.map((btn, i)=>{
                 return <CalcButton  key={i} symbol={btn.symbol} width={btn.width} action={(symbol)=>{btn.action(symbol)}}/>
