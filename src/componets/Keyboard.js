0f151e8f1a303bd134b50a99e1cecb7755c6bc0c
Aitor <aitordegomar91@hotmail.com>
2020-11-24 17:06:11 +0100

    removed buttons and show correctly text

--- src/componets/Keyboard.js
+++ src/componets/Keyboard.js
@@ -2,67 +2,63 @@ import React, {useState} from 'react'
 
 const Keyboard = () => {
     const [value, setValue] = useState('');
-    const vowels = ['a','e','i','o','u','y'];
-    const consonants = ['b','c','d','f','g','h','i','j','k','l','m','n','p','q','r','s','t','v','w','y','z'];
+    const [realValue, setRealValue] = useState('');
+    const v1 = ['a','e','i','o','u'];
+    const c1 = ['b','c','d','f','g','h','j','k','l','m','n','p','r','s','t','v','w','x', 'y','z', 'bl', 'cl','dr','fl','fr','gl','ch', 'll','pl','pr','qu','rr','tr'];
+    const v2 = v1;
+    const c2 = ['b','g','l','m','n','r','s','t','x','z'];
+
 
     const keyPressed = (e)=>{
         const code = e.charCodeAt(parseInt(value.length));
         if(code === 32){
             setValue(value + ' ')
         }else{
-        if(code < 97 || code > 122){
-            alert('Button not available');
-        }else{
-        if(Math.random()< 0.5){
-            setValue(value + vowels[Math.floor(Math.random() * (6))])
-        }else{
-            setValue(value + consonants[Math.floor(Math.random() * (21))])
-        }
+            if(code < 97 || code > 122){
+                alert('Button not available');
+            }else{
+                if(value.slice(-1) === ' '){
+                    if(v1.includes(value.slice(-2,-1))){
+                        setValue(value + c1[Math.floor(Math.random() * (33))]);
+                    }else{ 
+                        if(c1.includes(value.slice(-2,-1))){
+                            setValue(value + v1[Math.floor(Math.random() * (5))])
+                        }else{
+                            if(Math.random()< 0.5){
+                                setValue(value + v1[Math.floor(Math.random() * (5))])
+                            }else{
+                                setValue(value + c1[Math.floor(Math.random() * (33))])
+                            }
+                        }
+                    }
+                }else{
+                    if(v1.includes(value.slice(-2,-1))){
+                        setValue(value + c1[Math.floor(Math.random() * (34))]);
+                    }else{
+                        if(c1.includes(value.slice(-2,-1))){
+                            setValue(value + v1[Math.floor(Math.random() * (5))])
+                        }else{
+                            if(Math.random()< 0.5){
+                            setValue(value + v2[Math.floor(Math.random() * (5))])
+                            }else{
+                                setValue(value + c2[Math.floor(Math.random() * (10))])
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
-    }
-    
-    
 
     return (
         <div>
-            <input style={{display:'block'}} value={value || ''} onChange={(e)=>{keyPressed(e.target.value)}}/>
+            <input style={{display:'block'}} value={value || ''} onChange={(e)=>{
+                keyPressed(e.target.value);
+                setRealValue(realValue + e.nativeEvent.data);
+                }}/>
+            <input readOnly value={realValue || ''} />
             <button onClick={()=>setValue('')}>Clear</button>
-            <Keys letter='Q' />
-            <Keys letter='W' />
-            <Keys letter='E' />
-            <Keys letter='R' />
-            <Keys letter='T' />
-            <Keys letter='Y' />
-            <Keys letter='U' />
-            <Keys letter='I' />
-            <Keys letter='O' />
-            <Keys letter='P' />
-            <Keys letter='A' />
-            <Keys letter='S' />
-            <Keys letter='D' />
-            <Keys letter='F' />
-            <Keys letter='G' />
-            <Keys letter='H' />
-            <Keys letter='J' />
-            <Keys letter='K' />
-            <Keys letter='L' />
-            <Keys letter='Z' />
-            <Keys letter='C' />
-            <Keys letter='V' />
-            <Keys letter='B' />
-            <Keys letter='N' />
-            <Keys letter='M' />
         </div>
-    )
-}
-
-const Keys = (props)=>{
-    
-    return(
-        <>
-        <button type='button'>{props.letter}</button>
-        </>
     )
 }
 
